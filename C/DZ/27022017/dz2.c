ab29b2478bf2f70678573eddbdc795967a23287c
Pavel Novosadov <berk@mail.ru>
2017-02-27 17:36:49 +0300

    Create dz2.c

---
+++ C/DZ/27022017/dz2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <locale.h>
+#define SIZE_ARRAY 10
+
+
+
+/*
+Используя указатель на массив целых чисел, изменить порядок следования элементов
+массива на противоположный. Использовать в программе арифметику указателей для
+продвижения по массиву, а также оператор разыменования.
+*/
+
+void printArray(int* array, int* size);
+void smenaArray(int* array, int* size);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int size = SIZE_ARRAY;
+
+	int array[SIZE_ARRAY] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	
+
+	printArray(&array, &size);
+	
+
+	smenaArray(&array, &size);
+
+	printArray(&array, &size);
+	
+
+	return 0;
+}
+
+void printArray(int* array, int* size)
+{
+	for (int i = 0; i < *size; i++)
+	{
+		printf("%i ", *(array + i));
+	}
+	printf("\n");
+}
+
+
+
+void smenaArray(int* array, int* size)
+{
+	for (int i = 0, j = *size / 2; i<j; i++)
+	{
+		int d = *(array+i);
+		*(array + i) = *(array+(*size - 1 - i));
+		*(array + (*size - 1 - i)) = d;
+	}
+}
